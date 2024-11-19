65b1a858eb9da25bf75540138cd5f6100d41659c
Benben599 <bengadi.o@hotmail.com>
2024-11-19 11:48:19 +0100

    Feature: responsive

--- navbar.js
+++ navbar.js
@@ -1,9 +1,10 @@
 const box = document.querySelector('.box');
 const navLinks = document.querySelector(".nav-links");
-const footer = document.querySelector('footer')
+const footer = document.querySelector('footers-menu')
+
 
 box.addEventListener('click', () => {
   box.classList.toggle("active");
   navLinks.classList.toggle('mobile-menu');
-  footer.classList.toggle('footer-mobile');
+  footer.classList.toggle('footers-menu');
 })
