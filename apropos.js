fb5fa5193f15f42b2426ffcba28acca7e0ae51e7
Bouvier-Tobias <bouviertobiaspro@gmail.com>
2024-11-26 16:05:29 +0100

    Ajoue du responsive tablette page a propos

--- apropos.js
+++ apropos.js
@@ -4,9 +4,9 @@ const leftArrow = document.querySelector('#leftarrow');
 
 rightArrow.addEventListener('click', ()=>{
   slide.style.transform = 'translateX(-100vw)';
-  console.log('salut');
 })
 
 leftArrow.addEventListener('click',()=>{
   slide.style.transform = 'translateX(0)';
+  console.log('Salut');
 })
